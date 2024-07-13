a71921ceac85a9b995a021ab5d8f12b338c61bb7
Mike Schinkel <mike@newclarity.net>
2024-07-13 12:52:59 -0400

    Remove trailing slash from demo package in code

--- demo/main.php
+++ demo/main.php
@@ -2,8 +2,8 @@
 require "../src/autoload.php";
 UserlandPackages::register();
 
-require "phpkg://english-pkg/";
-require "phpkg://french-pkg/";
+require "phpkg://english-pkg";
+require "phpkg://french-pkg";
 
 $english = new English();
 $english->greeting();
